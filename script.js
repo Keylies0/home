3f884f4235dc35d60b262a892e7ff9286ba0a9c9
clement_petit <clement@debian.clement.com>
2025-09-09 20:19:02 +0200

    Themes clair et sombre avec localStorage

---
+++ script.js
@@ -0,0 +1,24 @@
+const btn_theme   = document.getElementById("btn_theme");
+const autre_theme = document.getElementById("autre_theme");
+
+function changer_theme() {
+    if (localStorage.getItem("theme") === "clair") {
+        localStorage.setItem("theme", "sombre");
+        document.body.className = "sombre"
+        autre_theme.textContent = "clair";
+    }
+    else {
+        localStorage.setItem("theme", "clair");
+        document.body.className = "clair"
+        autre_theme.textContent = "sombre";
+    }
+}
+
+if (localStorage.getItem("theme") === null)
+    localStorage.setItem("theme", "clair");
+else if (localStorage.getItem("theme") === "sombre") {
+    document.body.className = "sombre";
+    autre_theme.textContent = "clair";
+}
+
+btn_theme.addEventListener("click", changer_theme);
