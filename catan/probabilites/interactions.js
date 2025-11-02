8554366300f8a9a2e7d49199f30c599603251b76
clement_petit <clement@debian.clement.com>
2025-11-02 23:14:34 +0100

    ct moche, c mieux mtn

--- catan/probabilites/interactions.js
+++ catan/probabilites/interactions.js
@@ -14,7 +14,7 @@ function calculer_probabilites() {
         for (let i = 0; i < MAX_FIGURINES; i++) {
             const Figurinei = 'Figurine' + String(i);
             const figurine = document.getElementById(Figurinei);
-            if (figurine.value === 'Colonie' || figurine.value === 'Ville') {
+            if (FIGURINES.includes(figurine.value)) {
                 for (let j = 0; j < MAX_TUILES_INTERSECTION; j++) {
                     const ressource = document.getElementById(Figurinei + 'Ressource' + String(j));
                     if (ressource.value === r) {
@@ -48,7 +48,7 @@ function calculer_esperances() {
         for (let i = 0; i < MAX_FIGURINES; i++) {
             const Figurinei = 'Figurine' + String(i);
             const figurine = document.getElementById(Figurinei);
-            if (figurine.value != '') {
+            if (FIGURINES.includes(figurine.value)) {
                 for (let j = 0; j < MAX_TUILES_INTERSECTION; j++) {
                     const ressource = document.getElementById(Figurinei + 'Ressource' + String(j));
                     if (ressource.value === r) {
